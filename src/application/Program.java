8c80834be65bbe8d954f2c6a8b8cd8ab3ca475ee
IsmarleyA7X <ismarley_2005@msn.com>
2020-07-28 19:19:54 -0300

    Camada de xadres e imprime o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 		
 	}
 
