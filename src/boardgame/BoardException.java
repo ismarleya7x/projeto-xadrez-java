24f59ee5499eac3353268160c8514212aaeb83c3
IsmarleyA7X <ismarley_2005@msn.com>
2020-08-01 08:50:13 -0300

    Exceçao e programaçao defensiva

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+import javax.management.RuntimeErrorException;
+
+public class BoardException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public BoardException(String msg) {
+		super(msg);
+
+	}
+
+}
